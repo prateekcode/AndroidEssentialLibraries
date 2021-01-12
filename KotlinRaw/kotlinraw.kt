e53d3b268e77a7ba2d8a709c75a63b7cb0878b71
Prateek <codes.prateek@gmail.com>
2021-01-12 21:42:37 +0530

    Update kotlinraw.kt

--- KotlinRaw/kotlinraw.kt
+++ KotlinRaw/kotlinraw.kt
@@ -1,3 +1,22 @@
-fun main() {
-    println("Android Essential Libraries")
+fun main(args: Array<String>) {
+
+    var first = 1.20f
+    var second = 2.45f
+
+    println("--Before swap--")
+    println("First number = $first")
+    println("Second number = $second")
+
+    // Value of first is assigned to temporary
+    val temporary = first
+
+    // Value of second is assigned to first
+    first = second
+
+    // Value of temporary (which contains the initial value of first) is assigned to second
+    second = temporary
+
+    println("--After swap--")
+    println("First number = $first")
+    println("Second number = $second")
 }
